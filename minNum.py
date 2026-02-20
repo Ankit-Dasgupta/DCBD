34bc6f373328c62562740f5e476603b0e92cdcfc
Ankit-Dasgupta <dasguptaankit.1@gmail.com>
2026-02-20 16:49:21 +0530

    Update minNum.py

--- minNum.py
+++ minNum.py
@@ -1,7 +1,7 @@
-a = float(input("Enter first number: "))
+a = float(input("Enter first number: ")) 
 b = float(input("Enter second number: "))
 
-def minimum(a, b):
+def minimum(a, b): #giving minimum of two numbers
     return min(a, b)
 
 print("The minimum of the two numbers is", minimum(a,b))
